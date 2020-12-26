290a7ad16a9db565883fea4c60bfdd4eab768605
Wang-Songxiang <1374806247@qq.com>
2020-12-26 16:28:58 +0900

    BGMのon/off over　#2

---
+++ SupportersWinterHackathon/Assets/Scripts/BgmScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgmScript : MonoBehaviour
+{
+    public AudioSource source;
+    
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void bgmc(){
+        if(source.isPlaying){
+            source.Pause();
+            Debug.Log("pause");
+        }else{
+            source.Play();
+            Debug.Log("continue");
+        }
+    }
+}
