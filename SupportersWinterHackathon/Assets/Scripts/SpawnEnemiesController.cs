a17abf46a9456034f06bfd5bcfbd222140a50f69
Kdai58 <khydrm0508@gmail.com>
2020-12-26 17:39:02 +0900

    クリア条件の実装が完了．#6

--- SupportersWinterHackathon/Assets/Scripts/SpawnEnemiesController.cs
+++ SupportersWinterHackathon/Assets/Scripts/SpawnEnemiesController.cs
@@ -14,9 +14,9 @@ public class SpawnEnemiesController : MonoBehaviour
     }
 
     void SpawnEnemies() {
-        for (int i = 0; i < 10; i++) {
-            Vector3 spawnPoints = new Vector3(Random.Range(-2, 2),Random.Range(0, 2),Random.Range(-2, 2));
-            // Vector3 spawnPoints = new Vector3(0, -0.3f, 1);
+        for (int i = 0; i < 1; i++) {
+            // Vector3 spawnPoints = new Vector3(Random.Range(-2, 2),Random.Range(0, 2),Random.Range(-2, 2));
+            Vector3 spawnPoints = new Vector3(0, -0.3f, 1);
             Instantiate(virus, spawnPoints, Quaternion.identity);
             Debug.Log(spawnPoints);
         }
