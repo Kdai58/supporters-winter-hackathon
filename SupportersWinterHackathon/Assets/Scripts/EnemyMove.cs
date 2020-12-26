f4bf6a6b50064175c74d437166d7e940f0327d3a
Kdai58 <khydrm0508@gmail.com>
2020-12-26 22:00:18 +0900

    ゲームオーバー条件の実装が完了した．#17

--- SupportersWinterHackathon/Assets/Scripts/EnemyMove.cs
+++ SupportersWinterHackathon/Assets/Scripts/EnemyMove.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class EnemyMove : MonoBehaviour
 {
     public GameObject player;
@@ -11,14 +10,23 @@ public class EnemyMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectsWithTag("Player")[0];
         rb = GetComponent<Rigidbody>();
         playerPos = player.transform.position;
     }
-
     // Update is called once per frame
     void FixedUpdate()
     {
         dir = playerPos - this.transform.position;
         rb.AddForce(dir*speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.isTrigger == true)
+        {
+            Debug.Log("yay");
+            player.GetComponent<PlayerScript>().TakeDamage();
+        }
     }
 }
---
+++ SupportersWinterHackathon/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int HP;
+    public GameObject HPtext;
+    private Text UItext;
+    void Start(){
+        HP = 10;
+        UItext = HPtext.GetComponent<Text>();
+        UItext.text = "HP: " + HP.ToString();
+    }
+
+    public void TakeDamage(){
+        HP=HP-1;
+        UItext.text = "HP: " + HP.ToString();
+        if (HP <= 0){
+            Debug.Log("dead~");
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+}
