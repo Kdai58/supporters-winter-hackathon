1f093da13d4f76ab76aecc8d99efea12573c31d1
whutama <54239594+whutama@users.noreply.github.com>
2020-12-27 13:16:15 +0900

    PauseButtonのコードを追加

---
+++ SupportersWinterHackathon/Assets/Scripts/PauseButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour
+{
+    public GameObject shootbutton;
+    public GameObject crosshair;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void GamePauseResume()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            shootbutton.SetActive(false);
+            crosshair.SetActive(false);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            shootbutton.SetActive(true);
+            crosshair.SetActive(true);
+        }
+    }    
+}
